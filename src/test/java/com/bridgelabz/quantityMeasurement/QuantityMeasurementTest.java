123419d749c6d727eaaf540113de0722414d18b9
pushkarpm24 <pushkarmorey555@gmail.com>
2020-04-22 07:44:21 +0530

    ADDED:code to check refrence

--- src/test/java/com/bridgelabz/quantityMeasurement/QuantityMeasurementTest.java
+++ src/test/java/com/bridgelabz/quantityMeasurement/QuantityMeasurementTest.java
@@ -18,4 +18,10 @@ public class QuantityMeasurementTest {
         Feet feet2 = null;
         Assert.assertNotEquals(feet1, feet2);
     }
+
+    @Test
+    public void givenSameFeetRefrence_shouldReturnEqual() {
+        Feet feet = new Feet(0.0);
+        Assert.assertEquals(feet, feet);
+    }
 }
